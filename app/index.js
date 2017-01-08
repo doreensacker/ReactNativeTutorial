254d7fc05428e25dc5ed4c77ca062bf0727fdcbd
Doreen Sacker <dsacker@gmx.de>
2017-01-08 17:52:50 +0100

    finished

---
+++ app/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ListView
+} from 'react-native';
+
+export default class RNTutorial extends Component {
+  constructor(props){
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state={
+      text:'',
+      items: [],
+      dataSource: ds.cloneWithRows([])
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Todo App
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Gib eine Aufgabe ein'
+          onChangeText={(text) => this.setState({text})}/>
+        <Text
+          onPress={() => {
+            this.state.items.push(this.state.text);
+            this.setState({
+              dataSource: this.state.dataSource.cloneWithRows(this.state.items)
+            });
+          }}>
+          Save
+        </Text>
+        <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={(item) => <Text>{item}</Text>}
+          enableEmptySections={true}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textInput:{
+    height: 40,
+  },
+  listView: {
+    marginTop: 20
+  }
+});
+
+AppRegistry.registerComponent('RNTutorial', () => RNTutorial);
